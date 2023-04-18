22b1cca6a7deedc6e312964763130f1e0b219b7a
Euryn <crossdeltavi@gmail.com>
2023-04-18 20:31:55 +0200

    removed docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from "astro/config";
 import rehypeKatex from "rehype-katex";
 import remarkMath from "remark-math";
-
 import relativeLinks from "astro-relative-links";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eurydia.github.io",
-  base: "/practice-concurrent-clean/docs",
-  outDir: "docs/",
+  base: "/practice-concurrent-clean",
   markdown: {
     rehypePlugins: [rehypeKatex],
     remarkPlugins: [remarkMath],
