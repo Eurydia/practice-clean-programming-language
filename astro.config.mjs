78cce1f046fef160c73c7521b736f8bb53d037cc
Euryn <crossdeltavi@gmail.com>
2023-04-19 12:43:42 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
     remarkPlugins: [remarkMath],
     shikiConfig: {
       theme: "css-variables",
+      wrap: false,
     },
   },
   integrations: [relativeLinks()],
